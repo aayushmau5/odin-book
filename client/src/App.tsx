91314e157c06ba02284fc7f6bc224bd96ebcc31e
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-05-21 23:26:15 +0530

    Add icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
-import "./App.scss";
+import { FaBook } from "react-icons/fa";
+
+import styles from "./App.module.scss";
 import { Button } from "./Components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello, Odin</header>
-      <Button>Login</Button>
-      <Button>Signup</Button>
+    <div className={styles.app}>
+      <div className={styles.hero}>
+        <FaBook className={styles.icon} />
+        <header className={styles.header}>Odin Book</header>
+        <h3 className={styles.sub_header}>
+          Connect with your friends and families
+        </h3>
+        <h3 className={styles.sub_header}>Features:</h3>
+        <ol>
+          <li>Open Source</li>
+          <li>Privacy</li>
+          <li>No annoying ads and trackers</li>
+        </ol>
+      </div>
+      <div className={styles.form}>
+        <Button>Login</Button>
+      </div>
     </div>
   );
 }
--- client/src/Components/Button.tsx
+++ client/src/Components/Button.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  background: var(--gray);
+  background: var(--background);
   border-radius: 5px;
   border: none;
   cursor: pointer;
   color: var(--font);
-  outline-offset: 4px;
   font-family: inherit;
   margin: 10px;
   transition: 0.1s ease-in;
@@ -14,10 +13,8 @@ export const Button = styled.button`
   font-size: 1.2rem;
   &:hover {
     background: var(--light-red);
-    outline: 2px solid var(--light-red);
   }
   &:active {
     background: var(--red);
-    outline-color: var(--red);
   }
 `;
